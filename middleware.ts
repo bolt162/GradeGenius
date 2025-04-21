caab7f4f3994087568e2da2146bedfbea9b72b50
bolt162 <ash16299@gmail.com>
2025-04-20 20:13:51 -0500

    Fixing vercel issues

--- middleware.ts
+++ middleware.ts
@@ -30,8 +30,5 @@ export const config = {
      * - favicon.ico (favicon file)
      */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    
-    // Always run for API routes
-    '/api/(.*)'
   ],
 }; 
